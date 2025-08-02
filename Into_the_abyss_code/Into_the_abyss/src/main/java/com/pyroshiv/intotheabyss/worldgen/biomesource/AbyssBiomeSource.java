1d5dd0b955d072bfaa6342421989102af83f81a8
Evan Aubriet <89770263+Pyroshiv0@users.noreply.github.com>
2025-08-02 22:29:34 +0200

    Updated to fix repo with revert+ Modify AbyssBiomeSource.java

--- Into_the_abyss_code/Into_the_abyss/src/main/java/com/pyroshiv/intotheabyss/worldgen/biomesource/AbyssBiomeSource.java
+++ Into_the_abyss_code/Into_the_abyss/src/main/java/com/pyroshiv/intotheabyss/worldgen/biomesource/AbyssBiomeSource.java
@@ -7,12 +7,15 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.Climate;
 
-import java.io.Console;
+
 import java.util.stream.Stream;
 
+
 public class AbyssBiomeSource extends BiomeSource {
-    private int compt =10;
+    private static final int volcanoRadius=900;
+    private static final int adaptedVolcanoRadius=(volcanoRadius^ 2)/4;// on divise volcanoRadius par 4 car chaque x =4 correspond à x=1 dans les coordonées de jeu
     public static final Codec<AbyssBiomeSource> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Biome.CODEC.fieldOf("volcano_beach").forGetter(bs -> bs.volcanoBeach),
             Biome.CODEC.fieldOf("volcano_crater").forGetter(bs -> bs.volcanoCrater),
             Biome.CODEC.fieldOf("underground_jungle").forGetter(bs -> bs.undergroundJungle),
             Biome.CODEC.fieldOf("abyssal_ocean").forGetter(bs -> bs.abyssalOcean),
@@ -22,6 +25,7 @@ public class AbyssBiomeSource extends BiomeSource {
             Biome.CODEC.fieldOf("deepless_void").forGetter(bs -> bs.deeplessVoid)
     ).apply(instance, AbyssBiomeSource::new));
 
+    private final Holder<Biome> volcanoBeach;
     private final Holder<Biome> volcanoCrater;
     private final Holder<Biome> undergroundJungle;
     private final Holder<Biome> abyssalOcean;
@@ -30,7 +34,9 @@ public class AbyssBiomeSource extends BiomeSource {
     private final Holder<Biome> deepAbyss;
     private final Holder<Biome> deeplessVoid;
 
+
     public AbyssBiomeSource(
+            Holder<Biome> volcanoBeach,
             Holder<Biome> volcanoCrater,
             Holder<Biome> undergroundJungle,
             Holder<Biome> abyssalOcean,
@@ -40,6 +46,7 @@ public class AbyssBiomeSource extends BiomeSource {
             Holder<Biome> deeplessVoid
     ) {
         super();
+        this.volcanoBeach = volcanoBeach;
         this.volcanoCrater = volcanoCrater;
         this.undergroundJungle = undergroundJungle;
         this.abyssalOcean = abyssalOcean;
@@ -57,23 +64,28 @@ public class AbyssBiomeSource extends BiomeSource {
     @Override
     public Holder<Biome> getNoiseBiome(int x, int y, int z, Climate.Sampler sampler) {
         int blockY = y *4 ;//-1776 correspond au miny de ma dim
-        if (compt>0) {
-            compt--;
-            System.out.println("Valeur de blockY: " + blockY);
+
+
+        if (blockY >= -50 && blockY <= 250){
+            if (x*x +z*z <=(adaptedVolcanoRadius)){//pythagore
+                return volcanoCrater;
+            }
+            else{
+                return volcanoBeach;
+            }
         }
-        if (blockY >= -50 && blockY <= 250)
-            return volcanoCrater;
-        else if (blockY >= -350 && blockY < -50)
+
+        else if (blockY >= -350 && blockY <= -50)
             return undergroundJungle;
-        else if (blockY >= -550 && blockY < -350)
+        else if (blockY >= -550 && blockY <= -350)
             return abyssalOcean;
-        else if (blockY >= -700 && blockY < -550)
+        else if (blockY >= -700 && blockY <= -550)
             return twistedSea;
-        else if (blockY >= -1100 && blockY < -700)
+        else if (blockY >= -1100 && blockY <= -700)
             return hangingCliffs;
-        else if (blockY >= -1600 && blockY < -1100)
+        else if (blockY >= -1600 && blockY <= -1100)
             return deepAbyss;
-        else if (blockY >= -1800 && blockY < -1600)
+        else if (blockY >= -1800 && blockY <= -1600)
             return deeplessVoid;
         else
             return volcanoCrater;
@@ -82,6 +94,7 @@ public class AbyssBiomeSource extends BiomeSource {
     @Override
     public Stream<Holder<Biome>> collectPossibleBiomes() {
         return Stream.of(
+                volcanoBeach,
                 volcanoCrater, undergroundJungle, abyssalOcean,
                 twistedSea, hangingCliffs, deepAbyss, deeplessVoid
         );
