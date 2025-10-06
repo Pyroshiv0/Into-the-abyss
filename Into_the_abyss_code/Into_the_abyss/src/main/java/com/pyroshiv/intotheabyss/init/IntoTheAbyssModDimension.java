f94b5cc5b10b28b4c79147ea1bd1d6028e3c62a6
Evan Aubriet <89770263+Pyroshiv0@users.noreply.github.com>
2025-10-06 22:25:00 +0200

    Added a Chunk Generator and a world preset for the dimension. Long time no see(/commmit)

--- Into_the_abyss_code/Into_the_abyss/src/main/java/com/pyroshiv/intotheabyss/init/IntoTheAbyssModDimension.java
+++ Into_the_abyss_code/Into_the_abyss/src/main/java/com/pyroshiv/intotheabyss/init/IntoTheAbyssModDimension.java
@@ -1,5 +1,6 @@
 package com.pyroshiv.intotheabyss.init;
 
+import com.pyroshiv.intotheabyss.IntoTheAbyssMod;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -10,18 +11,20 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
+import static com.pyroshiv.intotheabyss.IntoTheAbyssMod.MODID;
+
 public class IntoTheAbyssModDimension {
     public static final DeferredRegister<DimensionType> REGISTRY =
-        DeferredRegister.create(Registries.DIMENSION_TYPE, "into_the_abyss");
+        DeferredRegister.create(Registries.DIMENSION_TYPE, MODID);
     public static final ResourceKey<DimensionType> THE_ABYSS_KEY =
-            ResourceKey.create(Registries.DIMENSION_TYPE, new ResourceLocation("into_the_abyss", "the_abyss"));
+            ResourceKey.create(Registries.DIMENSION_TYPE, new ResourceLocation(MODID, "the_abyss"));
 
 
     public static final ResourceKey<Level> THE_ABYSS_DIM =
-            ResourceKey.create(Registries.DIMENSION, new ResourceLocation("into_the_abyss", "the_abyss"));
+            ResourceKey.create(Registries.DIMENSION, new ResourceLocation(MODID, "the_abyss"));
 
 
-    @Mod.EventBusSubscriber(modid = "into_the_abyss", bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public class DimensionInit {
         @SubscribeEvent
         public static void onRegister(RegisterEvent event) {
